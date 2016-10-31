bc24471d0800e44d6882c586a8e35ac8861b5d92
Haonan Song <haonan-song@uiowa.edu>
2016-10-31 11:41:08 -0500

    Faster way (dynamic programming)

--- src/Solution_338.java
+++ src/Solution_338.java
@@ -2,26 +2,17 @@
  * Created by SHN on 10/30/16.
  */
 public class Solution_338 {
-    public int[] countBits2(int num) {
-          int i = 0;
-        int j = 2;
-        int counter = 0;
+    public int[] countBits(int num) {
+
         int[] res = new int[num+1];
-        while( i <= num){
-            if(i < j) counter += i%2;
-            else{
-                j*=2;
-                counter = 1;
-            }
-            res[i] = counter;
-            i++;
+        for(int i = 0; i <= num ;i++){
+            res[i] = (i%2 == 0)? res[i/2] : res[i-1]+1;
         }
         return res;
-
     }
 
 
-    public int[] countBits(int num) {
+    public int[] countBits2(int num) {
 
         int[] res = new int[num+1];
         for(int i = 0; i <= num ;i++){
