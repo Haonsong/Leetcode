f721f7100e8387d083b754a786e28c0b85627ded
Haonan Song <haonan-song@uiowa.edu>
2016-11-01 11:55:09 -0500

    Finished problem 441

--- src/Solution_441.java
+++ src/Solution_441.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
-
 /**
  * Created by SHN on 11/1/16.
  */
 public class Solution_441 {
+    public int arrangeCoins(int n) {
+        int i = 1;
+        while(n>i){
+            n-= i;
+            i++;
+        }
+
+        if(n > 0) return i-1;
+        return i;
+    }
 }
+
+/*
+
+Test cases
+1
+5
+8
+9
+10
+22
+100
+ */
