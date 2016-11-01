d6e4217ebd1ff011b5d6409d0ce9305af4b3b6d0
Haonan Song <haonan-song@uiowa.edu>
2016-11-01 12:14:19 -0500

    Update O(1) solution for Problem 441

--- src/Solution_441.java
+++ src/Solution_441.java
@@ -2,7 +2,13 @@
  * Created by SHN on 11/1/16.
  */
 public class Solution_441 {
+
     public int arrangeCoins(int n) {
+        // covert to long type to solve the overflow, (1+(1+(n-1)*d)*n/2 = sum of arithmetic seq
+        return (int) (Math.sqrt( (2*(long)n) + 0.25) -0.5);
+    }
+
+    public int arrangeCoins2(int n) {
         int i = 1;
         while(n>=i){
             n-= i;
