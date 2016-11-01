b50dab9bc52233dffc9de19d1736d858c44a6a6b
Haonan Song <haonan-song@uiowa.edu>
2016-11-01 13:24:05 -0500

    Update the solution 438 with solution from community

--- src/Solution_438.java
+++ src/Solution_438.java
@@ -35,18 +35,28 @@ public class Solution_438 {
 
 
     public List<Integer> findAnagrams(String s, String p) {
+        int[] alpha = new  int[26];
         ArrayList<Integer> res =  new ArrayList<>();
 
-//        char[] source_Array = s.toCharArray();
-        String target_Letters = to_letters(p);
-        int p_size = p.length();
-//        HashMap<String,>
+        for(char c: p.toCharArray())
+            alpha[c-'a'] ++;
 
-        for(int i = 0; i< s.length()-p_size+1; i++ ){
-            String sub = to_letters(s.substring(i,i+p_size));
-            if(sub.equals(target_Letters))
-                res.add(i);
+        int lh = 0, rh = 0, p_size = p.length();
+
+
+        while (rh < s.length()){
+            if (rh - lh >= p.length()){
+                int tmp = alpha[s.charAt(lh++) - 'a']++;
+                if(tmp>=0) p_size++;
+            }
+            if(--alpha[s.charAt(rh++)-'a'] >= 0)
+                p_size--;
+
+            if (p_size == 0)
+                res.add(lh);
         }
+
+
         return res;
     }
 }
